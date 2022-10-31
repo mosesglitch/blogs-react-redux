5b0bdab518a0cc19de484279745d6ea6e3f6d6ef
mosesglitch <mosesglitch>
2022-10-31 22:26:37 +0300

    add api

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions";
 
 class PostList extends Component {
   render() {
@@ -6,4 +8,4 @@ class PostList extends Component {
   }
 }
 
-export default PostList;
+export default connect(null, { fetchPosts })(PostList);
